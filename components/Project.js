d7da02301983cd13728e1bb02b982e273da6c195
lavgup <lavyag01@gmail.com>
2021-05-01 21:19:21 +1000

    Add sequential headings

--- components/Project.js
+++ components/Project.js
@@ -11,9 +11,9 @@ export default function Project({ title, description, tag, href }) {
 		>
 			<div className="items-center border border-gray-200 dark:border-gray-800 rounded p-4">
 				<div className="flex justify-start items-start">
-					<h4 className="leading-6 text-lg font-bold tracking-tight text-gray-900 dark:text-gray-100">
+					<h3 className="leading-6 text-lg font-bold tracking-tight text-gray-900 dark:text-gray-100">
 						{title}
-					</h4>
+					</h3>
 					<div
 						className="rounded text-gray-500 bg-gray-200 dark:bg-gray-600 dark:text-gray-300 px-1 ml-2">
 						<p className="text-sm">
