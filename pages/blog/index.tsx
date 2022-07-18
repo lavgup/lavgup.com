67a69c70c5f96fa011884e0f2eff203105fa44fb
lavgup <lavyag01@gmail.com>
2022-07-18 16:16:15 +1000

    Add post count

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -18,8 +18,8 @@ export interface BlogPost {
 }
 
 export default function Blog({
-    posts
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+                                 posts
+                             }: InferGetStaticPropsType<typeof getStaticProps>) {
     const [searchValue, setSearchValue] = useState('');
 
     const tags = posts.map((p) => p.tags).flat();
@@ -38,24 +38,25 @@ export default function Blog({
     }
 
     return (
-        <Container title="Blog - Lav" description={description} rss>
-            <h1 className="text-4xl font-bold">Blog</h1>
-            <p className="mt-3">
+        <Container title='Blog - Lav' description={description} rss>
+            <h1 className='text-4xl font-bold'>Blog</h1>
+            <p className='mt-3'>
                 Discussion on my personal thoughts, projects and hobbies.
             </p>
 
-            <div className="relative items-center">
-                <SearchIcon className="absolute text-gray-400 w-[1.15rem] h-[1.15rem] left-2 top-[1.275rem] dark:text-gray-300" />
+            <div className='relative items-center'>
+                <SearchIcon
+                    className='absolute text-gray-400 w-[1.15rem] h-[1.15rem] left-2 top-[1.275rem] dark:text-gray-300' />
 
                 <input
-                    type="search"
-                    className="py-1 px-1.5 pl-8 mt-3 w-96 max-w-full bg-transparent rounded-md border border-md border-orange-200/40 dark:border-orange-400/20"
-                    placeholder="Search"
+                    type='search'
+                    className='py-1 px-1.5 pl-8 mt-3 w-96 max-w-full bg-transparent rounded-md border border-md border-orange-200/40 dark:border-orange-400/20'
+                    placeholder='Search'
                     onChange={(e) => setSearchValue(e.target.value)}
                 />
             </div>
 
-            <ol className="flex flex-row flex-wrap gap-2 mt-5">
+            <ol className='flex flex-row flex-wrap gap-2 mt-5'>
                 {tags.map((el, idx) => (
                     <li key={idx}>
                         <Tag tag={el} />
@@ -63,15 +64,19 @@ export default function Blog({
                 ))}
             </ol>
 
-            <div className="flex flex-row items-baseline mt-10">
-                <h1 className="text-3xl font-bold">All Posts</h1>
+            <div className='flex flex-row items-baseline mt-10'>
+                <h1 className='text-3xl font-bold'>All Posts</h1>
+                <span className='text-lg font-bold ml-1.5 -translate-y-[.275rem]'>({sorted.length})</span>
             </div>
             <ol>
-                {sorted.map((post, idx) => (
-                    <li key={idx} className="mt-3">
-                        <BlogCard post={post} />
-                    </li>
-                ))}
+                {sorted.length > 0
+                    ? sorted.map((post, idx) => (
+                        <li key={idx} className='mt-3'>
+                            <BlogCard post={post} />
+                        </li>
+                    ))
+                    : <p className="text-md mt-2">There are no posts to display.</p>
+                }
             </ol>
         </Container>
     );
